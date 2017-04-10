10c10127ec5c5dfcd07b3f8929469e1e6c452e26
vcwai <victorcwai@gmail.com>
2017-04-10 21:27:06 +0800

    Changed from centralized to distributed structure
    No more master server, but client needs to add addresses of volume servers before they start
    Chunks URLs no longer reside in master
    In order to get URLs of chunks, client will use a hash function to locate it

--- client/client.go
+++ client/client.go
@@ -15,11 +15,13 @@ import (
 	"bytes"
 	"sync"
 	"encoding/gob"
-	"math/rand"
+	// "math/rand"
+	"hash/fnv"
 )
 
 const BUFFERSIZE = 65536
 const CHUNKSIZE int64 = 134217728 //2^27
+var volumeServerList = []string{"localhost:2001","localhost:2002"}
 
 type receivedBlock struct {
     data []byte
@@ -29,25 +31,24 @@ type receivedBlock struct {
 func main() {	
 		//connection to master
 		start := time.Now()
-		connection, err := net.Dial("tcp", os.Args[1])
-		fmt.Println(os.Args[1])
-		if err != nil {
-			fmt.Println("There was an error making a connection")
-		}
+		//connection, err := net.Dial("tcp", os.Args[1])
+		// if err != nil {
+		// 	fmt.Println("There was an error making a connection")
+		// }
 
-		if os.Args[2] == "send" {
-			SendFileToServer(os.Args[3], connection, start)
-		} else if os.Args[2] == "rece" {
-			GetFileFromServer(os.Args[3], connection, start)
-		} else if os.Args[2] == "dele" {
-			DeleteFileInServer(os.Args[3], connection, start)
+		if os.Args[1] == "send" {
+			SendFileToServer(os.Args[2], start)
+		} else if os.Args[1] == "rece" {
+			GetFileFromServer(os.Args[2], start)
+		} else if os.Args[1] == "dele" {
+			DeleteFileInServer(os.Args[2], start)
 		} else {
 			fmt.Println("Bad Command")
 		}
 	//}
 }
 
-func SendFileToServer(fileName string, connection net.Conn, start time.Time) {
+func SendFileToServer(fileName string, start time.Time) {
 
 	fmt.Println("Send to server")
 	var err error
@@ -56,7 +57,7 @@ func SendFileToServer(fileName string, connection net.Conn, start time.Time) {
 	file, err := os.Open(fileName) // For read access.
 	fmt.Println(fileName)
 	if err != nil {
-		connection.Write([]byte("-1"))
+		// connection.Write([]byte("-1"))
 		log.Fatal(err)
 	}
 
@@ -65,13 +66,14 @@ func SendFileToServer(fileName string, connection net.Conn, start time.Time) {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println("Sending command to master")
-	connection.Write([]byte("send"))
-	enc := gob.NewEncoder(connection) // Will write to network.
-	dec := gob.NewDecoder(connection) // Will read from network.
-	var volumeServerMap map[int]string
-	dec.Decode(&volumeServerMap)
-	fmt.Println(volumeServerMap)
+
+	// fmt.Println("Sending command to master server.")
+	// connection.Write([]byte("send"))
+	// enc := gob.NewEncoder(connection) // Will write to network.
+	// dec := gob.NewDecoder(connection) // Will read from network.
+	// var volumeServerList map[int]string
+	// dec.Decode(&volumeServerList)
+	// fmt.Println(volumeServerList)
 	chunkCount := 0
 
 	// file.Close()
@@ -79,68 +81,63 @@ func SendFileToServer(fileName string, connection net.Conn, start time.Time) {
 	for i := fileInfo.Size(); i > 0; i -= CHUNKSIZE{
 		chunkCount+=1
 	}
-	fmt.Println("chunkCount=",chunkCount)
 	chunkList := make([]string, chunkCount+1)
 	chunkList[0] = fileInfo.Name()
-	fmt.Println("Start sending file")
 
-	// var wg sync.WaitGroup
-	// maxGoroutines := 8
- //    guard := make(chan struct{}, maxGoroutines)
+	fmt.Println("chunkCount=",chunkCount)
+	fmt.Println("Start sending file")
 
 	//connect to node, for each chunk, send to different volume server
 	for i:= 0; i < chunkCount; i += 1{
 		//send until 1 chunk is completed, then move on to next chunk and volume server
-		// wg.Add(1)
-		// go func(i int){
-		// 	defer wg.Done()
-		// 	guard <- struct{}{} // would block if guard channel is already filled
-			index := rand.Intn(len(volumeServerMap)) //random load-balancing
-			fmt.Println("index:",index)
-			sendBuffer := make([]byte, BUFFERSIZE)
-			vsConnection, _ := net.Dial("tcp", volumeServerMap[index])
-			
-			vsConnection.Write([]byte("send"))
-			
-			chunkList[i+1] = volumeServerMap[index]
-			
-			chunkName := fileInfo.Name()+strconv.Itoa(i)
-			vsConnection.Write([]byte(fillString(chunkName, 64)))
-			fmt.Println("chunkName:",chunkName)        
-			var sentByte int64
-			sentByte=0
-			// file, _ := os.Open(fileName)
-   //      	file.Seek(int64(i)*CHUNKSIZE,0) //CHUNKSIZE is 134217728
-			for { 
-				n, err := file.Read(sendBuffer)
-				if err != nil {
-					fmt.Println("err",err,chunkName)
-					break
-				}
+		chunkName := fileInfo.Name()+strconv.Itoa(i)
+		index := hash(chunkName)% uint32(len(volumeServerList))
+		
+		fmt.Println("chunkName:",chunkName)  
+		fmt.Println("index:",index)
 
-				n2, err2 := vsConnection.Write(sendBuffer[:n])
-				if err2 != nil {
-					fmt.Println("err2",err2,chunkName)
-					break
-				}
-				if(n2!=65536){
-					fmt.Println("n2",n2)
-				}
-				sentByte = sentByte+int64(n)
-				if(sentByte == CHUNKSIZE){
-					break;
-				}
+		sendBuffer := make([]byte, BUFFERSIZE)
+		vsConnection, _ := net.Dial("tcp", volumeServerList[index])
+		fmt.Println(volumeServerList[index])
+		vsConnection.Write([]byte("send"))
+		
+		chunkList[i+1] = volumeServerList[index]
+		
+		vsConnection.Write([]byte(fillString(chunkName, 64)))      
+		var sentByte int64
+		sentByte=0
+		for { 
+			n, err := file.Read(sendBuffer)
+			if err != nil {
+				fmt.Println("err",err,chunkName)
+				break
 			}
-			fmt.Println("Finished sending",chunkName)
-			vsConnection.Close()
-			// wg.Done()
-		// 	<-guard
-		// }(i)
+
+			n2, err2 := vsConnection.Write(sendBuffer[:n])
+			if err2 != nil {
+				fmt.Println("err2",err2,chunkName)
+				break
+			}
+			if(n2!=65536){
+				fmt.Println("n2",n2)
+			}
+			sentByte = sentByte+int64(n)
+			if(sentByte == CHUNKSIZE){
+				break;
+			}
+		}
+		fmt.Println("Finished sending",chunkName)
+		vsConnection.Close()
 	}
-	// wg.Wait()
 	fmt.Println("Finished sending.")
-	enc.Encode(chunkList)
 	file.Close()
+			
+	//send chunkList to a server
+	index2 := hash(fileName)%uint32(len(volumeServerList))
+	connection, _ := net.Dial("tcp", volumeServerList[index2])
+	connection.Write([]byte("setL"))
+	enc := gob.NewEncoder(connection)
+	enc.Encode(chunkList)
 	connection.Close()
 
 	elapsed := time.Since(start)
@@ -148,10 +145,13 @@ func SendFileToServer(fileName string, connection net.Conn, start time.Time) {
     return
 }
 
-func GetFileFromServer(fileName string, connection net.Conn, start time.Time) {
-    fmt.Println("receive from server")
+func GetFileFromServer(fileName string, start time.Time) {
+    fmt.Println("Receive from server")
+	
+	index := hash(fileName)%uint32(len(volumeServerList))
+	connection, _ := net.Dial("tcp", volumeServerList[index])
 
-	connection.Write([]byte("rece"))
+	connection.Write([]byte("getL"))
 
 	enc := gob.NewEncoder(connection)
 	enc.Encode(fileName)
@@ -163,12 +163,14 @@ func GetFileFromServer(fileName string, connection net.Conn, start time.Time) {
 		fmt.Println("File not found.")
 		return
 	}
+	connection.Close()
+
 	fmt.Println("chunkList:",chunkList)
 
 	c := make(chan *receivedBlock)
 	var wg sync.WaitGroup
 	wg.Add(1)
-	go BlockAssembler(c,len(chunkList),fileName,&wg,start)
+	go blockAssembler(c,len(chunkList),fileName,&wg,start)
 	
 	maxGoroutines := 3
     guard := make(chan struct{}, maxGoroutines)
@@ -213,7 +215,6 @@ func GetFileFromServer(fileName string, connection net.Conn, start time.Time) {
 	fmt.Println("Receiving file complete.")
 
 	// file.Close()
-	connection.Close()
     fmt.Println("Finished receiving.")
 	elapsed := time.Since(start)
     fmt.Printf("Receiving flie in disk took %s\n", elapsed)
@@ -221,17 +222,21 @@ func GetFileFromServer(fileName string, connection net.Conn, start time.Time) {
 
 }
 
-func DeleteFileInServer(fileName string, connection net.Conn, start time.Time) {	    
-	fmt.Println("receive from server")
+func DeleteFileInServer(fileName string, start time.Time) {	    
+	fmt.Println("Delete from server")
+	
+	index := hash(fileName)%uint32(len(volumeServerList))
+	connection, _ := net.Dial("tcp", volumeServerList[index])
 
-	connection.Write([]byte("dele"))
+	connection.Write([]byte("getL"))
 
-	enc := gob.NewEncoder(connection) 
-	enc.Encode(fileName)
+	// enc := gob.NewEncoder(connection) 
+	// enc.Encode(fileName)
 	
 	var chunkList []string
 	dec := gob.NewDecoder(connection)
 	dec.Decode(&chunkList)
+	connection.Close()
 
 	fmt.Println("chunkList:",chunkList)
 	for i:= 0; i < len(chunkList); i += 1{
@@ -245,7 +250,6 @@ func DeleteFileInServer(fileName string, connection net.Conn, start time.Time) {
 	}
 	fmt.Println("Deleting file complete.")
 
-	connection.Close()
     fmt.Println("Finished deleting.")
     elapsed := time.Since(start)
     fmt.Printf("Sending request for deleting flie took %s\n", elapsed)
@@ -254,7 +258,7 @@ func DeleteFileInServer(fileName string, connection net.Conn, start time.Time) {
 
 }
 
-func BlockAssembler(c chan *receivedBlock, chunkNum int, fileName string, wg *sync.WaitGroup,start time.Time){
+func blockAssembler(c chan *receivedBlock, chunkNum int, fileName string, wg *sync.WaitGroup,start time.Time){
 	defer wg.Done()
 	receivedBlockList := make([]*receivedBlock, chunkNum)
 	
@@ -304,6 +308,12 @@ func BlockAssembler(c chan *receivedBlock, chunkNum int, fileName string, wg *sy
 	fmt.Println("Block Assembler done.")
     elapsed := time.Since(start)
     fmt.Printf("Receiving file in memory took %s\n", elapsed)
+}
+
+func hash(s string) uint32 {
+        h := fnv.New32a()
+        h.Write([]byte(s))
+        return h.Sum32()
 }
 
 func fillString(returnString string, toLength int) string {
--- volumeServer/server.go
+++ volumeServer/server.go
@@ -7,14 +7,14 @@ import (
 	"fmt"
 	"io"
 	"net"
-	"runtime"
+	// "runtime"
 	// "strconv"
 	"bytes"
 	"strings"
 	"time"
 	"log"
 	"os"
-	// "encoding/gob"
+	"encoding/gob"
 	"github.com/streamrail/concurrent-map"
 )
 
@@ -22,8 +22,8 @@ const BUFFERSIZE = 65536
 const CHUNKSIZE int64 = 134217728 //2^27
 
 func main() {
-    mapping := cmap.New()
-
+    mapping := cmap.New() //{chunk id, chunk bytes}
+	fileChunkMap := cmap.New() //{file id, {chunks url}}
 	server, error := net.Listen("tcp", os.Args[1])
 	if error != nil {
 		fmt.Println("There was an error starting the server" + error.Error())
@@ -41,12 +41,13 @@ func main() {
 			return
 		}
 		//one goroutine per connection
-		go ConnectionHandler(connection,mapping)
+		go connectionHandler(connection,mapping,fileChunkMap)
 
 	}
 }
 
-func ConnectionHandler(connection net.Conn, mapping cmap.ConcurrentMap) {
+func connectionHandler(connection net.Conn, mapping cmap.ConcurrentMap, fileChunkMap cmap.ConcurrentMap) {
+	start := time.Now()
 	fmt.Println("Connected")
 
 	bufferCommand := make([]byte, 4)
@@ -54,91 +55,151 @@ func ConnectionHandler(connection net.Conn, mapping cmap.ConcurrentMap) {
 
 	connection.Read(bufferCommand)
 	command := string(bufferCommand)
+	fmt.Println("Command is: " + command)
 
+	//handle reuqest of sending/getting chunk list
+	if(command == "setL"){
+		setChunkList(connection,fileChunkMap,start)
+		return
+	}else if(command == "getL"){
+		getChunkList(connection,fileChunkMap,start)
+		return
+	}
+
+	//handle request of sending/getting chunks
 	connection.Read(bufferChunkName)
 	chunkName := string(bufferChunkName)
 	chunkName = strings.Trim(string(chunkName), ":")
 
-	fmt.Println("Command is: " + command)
 	fmt.Println("chunkName is: " + chunkName)
 	
-	start := time.Now()
 	if (command == "send"){
-		//make a buffer to hold data		
-	    var bufferFile bytes.Buffer
-	    writer := bufio.NewWriter(&bufferFile)
-
-		var receivedBytes int64
-	    receivedBytes=0
-		for {
-
-			if(CHUNKSIZE<=receivedBytes){
-				break
-			}
-			n,err := io.CopyN(writer, connection, BUFFERSIZE)
-			receivedBytes += n
-			if err != nil {
-				fmt.Println("err", err.Error(), chunkName)
-				break
-			}
-		}
-		mapping.Set(chunkName,bufferFile.Bytes())
-		elapsed := time.Since(start)
-	    fmt.Printf("Storing file took %s\n", elapsed)
-
-		for k := range mapping.Iter() {
-        	fmt.Printf("%s\n", k.Key)
-    	}
-		var m runtime.MemStats  
-	    runtime.ReadMemStats(&m)
-	    fmt.Printf("%d,%d,%d,%d\n", m.HeapSys, m.HeapAlloc, m.HeapIdle, m.HeapReleased)
-		log.Println("Memory Acquired: ", m.Sys)
-		log.Println("Memory Used    : ", m.Alloc)
-
-		connection.Close()
-
+		receiveChunk(chunkName,connection,mapping,start)
+		return
 	}else if (command == "rece"){
-		elapsed := time.Since(start)
-	    fmt.Printf("File access took %s\n", elapsed)
-	    fileData,_ := mapping.Get(chunkName)
-	    fileBytes := fileData.([]byte)
-		reader := bytes.NewReader(fileBytes)
-		fileSize := len(fileBytes)
-		sendBuffer := make([]byte,BUFFERSIZE)
-
-		var sentBytes int64
-		for {
-			if (int64(fileSize) - sentBytes <= 0){
-				break
-			}
-			n,err := reader.Read(sendBuffer)			
-			if err == io.EOF {
-				break
-			}
-
-			_,err2 := connection.Write(sendBuffer[:n])
-			if err2 != nil {
-				fmt.Println("err2", err2.Error(), chunkName)
-				break
-			}
-			sentBytes += int64(n)
-		}
-
-		connection.Close()
-		fmt.Println("Finished sending.")
-
+		sendChunk(chunkName,connection,mapping,start)
+		return
 	}else if (command == "dele") {
-		mapping.Remove(chunkName)
-		fmt.Println("Chunk", chunkName ,"deleted successfully.")
+		removeChunk(chunkName,mapping)
+		connection.Close()
+		return
 	}else {
-		fmt.Println("Bad command received, ConnectionHandler finished")
-		return;
+		fmt.Println("Bad command received, connectionHandler finished")
+		connection.Close()
+		return
 	}
-
-    fmt.Println("Request finished.")
-// }
 }
 
+func setChunkList(connection net.Conn, fileChunkMap cmap.ConcurrentMap, start time.Time){
+	//then receive chunkList from client and update fileChunkMap
+    dec := gob.NewDecoder(connection)
+	
+    var chunkList []string
+
+    dec.Decode(&chunkList)
+    fmt.Printf("chunkList received : %v\n", chunkList);
+
+    fileChunkMap.Set(chunkList[0],chunkList[1:]) //chunkList[0] = ID
+    connection.Close()
+    
+	fmt.Println(fileChunkMap)
+
+	elapsed := time.Since(start)
+	fmt.Printf("Request took %s\n", elapsed)
+}
+
+func getChunkList(connection net.Conn, fileChunkMap cmap.ConcurrentMap, start time.Time){
+
+	//send chunks location (fileChunkMap[ID]) to client
+	var fileName string
+	dec := gob.NewDecoder(connection)
+	dec.Decode(&fileName)
+
+	fmt.Println("File name is: " + fileName)
+
+	enc := gob.NewEncoder(connection)
+	chunkURLs,ok := fileChunkMap.Get(fileName)
+	if(ok==false){
+		fmt.Println("File not found. Request finished.")
+		return
+	}
+	err := enc.Encode(chunkURLs)
+    if err != nil {
+    	log.Fatal("encode error:", err)
+    }
+	elapsed := time.Since(start)
+	fmt.Printf("Request took %s\n", elapsed)
+}
+
+func receiveChunk(chunkName string, connection net.Conn, mapping cmap.ConcurrentMap, start time.Time){
+	//make a buffer to hold data		
+    var bufferFile bytes.Buffer
+    writer := bufio.NewWriter(&bufferFile)
+
+	var receivedBytes int64
+    receivedBytes=0
+	for {
+
+		if(CHUNKSIZE<=receivedBytes){
+			break
+		}
+		n,err := io.CopyN(writer, connection, BUFFERSIZE)
+		receivedBytes += n
+		if err != nil {
+			fmt.Println("err", err.Error(), chunkName)
+			break
+		}
+	}
+	mapping.Set(chunkName,bufferFile.Bytes())
+	elapsed := time.Since(start)
+    fmt.Printf("Storing file took %s\n", elapsed)
+
+	for k := range mapping.Iter() {
+    	fmt.Printf("%s\n", k.Key)
+	}
+
+	// var m runtime.MemStats  
+ //    runtime.ReadMemStats(&m)
+ //    fmt.Printf("%d,%d,%d,%d\n", m.HeapSys, m.HeapAlloc, m.HeapIdle, m.HeapReleased)
+	// log.Println("Memory Acquired: ", m.Sys)
+	// log.Println("Memory Used    : ", m.Alloc)
+	connection.Close()
+}
+
+func sendChunk(chunkName string, connection net.Conn, mapping cmap.ConcurrentMap, start time.Time){
+	elapsed := time.Since(start)
+    fmt.Printf("File access took %s\n", elapsed)
+    fileData,_ := mapping.Get(chunkName)
+    fileBytes := fileData.([]byte)
+	reader := bytes.NewReader(fileBytes)
+	fileSize := len(fileBytes)
+	sendBuffer := make([]byte,BUFFERSIZE)
+
+	var sentBytes int64
+	for {
+		if (int64(fileSize) - sentBytes <= 0){
+			break
+		}
+		n,err := reader.Read(sendBuffer)			
+		if err == io.EOF {
+			break
+		}
+
+		_,err2 := connection.Write(sendBuffer[:n])
+		if err2 != nil {
+			fmt.Println("err2", err2.Error(), chunkName)
+			break
+		}
+		sentBytes += int64(n)
+	}
+	fmt.Println("Finished sending.")
+	connection.Close()
+}
+
+func removeChunk(chunkName string, mapping cmap.ConcurrentMap){
+	mapping.Remove(chunkName)
+	fmt.Println("Chunk", chunkName ,"deleted successfully.")
+}
 
 func fillString(returnString string, toLength int) string {
 	byteBuffer := bytes.NewBufferString(returnString)
