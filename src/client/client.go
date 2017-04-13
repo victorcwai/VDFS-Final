c174080c2a78562a459da7d92fe5bd1f840592bd
vcwai <victorcwai@gmail.com>
2017-04-13 21:28:10 +0800

    Update client.go

--- src/client/client.go
+++ src/client/client.go
@@ -23,7 +23,7 @@ import (
 const BUFFERSIZE = 65536
 const CHUNKSIZE int64 = 134217728 //2^27
 var volumeServerList = []string{"10.140.0.2:2001","10.140.0.3:2001","10.140.0.4:2001","10.140.0.5:2001","10.140.0.7:2001"}
-var hasher Hasher = jump.New(len(volumeServerList), jump.FNV1a)
+var hasher *jump.Hasher = jump.New(len(volumeServerList), jump.FNV1a)
 
 type receivedBlock struct {
     data []byte
